811b3a5fd1894b0d50b9cc0a496bdc8affa8cfb3
sfu <sfu.concepts@gmail.com>
2017-01-07 16:43:29 -0500

    create images folder in frontend

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,12 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// var App = React.createClass({
-// 	render(){
-// 		return(
-// 			<p>hello world</p>
-// 			)
-// 	}
-// })
+var App = React.createClass({
+	render(){
+		return(
+			<p>hello world</p>
+			)
+	}
+})
 
-// ReactDOM.render(<App />, document.getElementById('root'))
-console.log("hello")
+ReactDOM.render(<App />, document.getElementById('root'))
